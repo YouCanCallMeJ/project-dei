40d447da213bfa78a395c7470187fc23aef6bf9d
Ina Yoon <yia0319@gmail.com>
2022-09-21 12:55:19 -0400

    Added Link components to imgs

--- src/App.js
+++ src/App.js
@@ -22,46 +22,32 @@ function App(){
       </Navbar>
       <div className="main-bg"></div>
 
-      <CardGroup>
+    <CardGroup>
       <Card>
-        <Card.Img variant="top" className="card1" />
+        <Link to="/quotes">
+          <Card.Img variant="top" className="card1" />
+        </Link>
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
+          <Card.Title>Relaxing quotes You Really Need</Card.Title>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
+
       <Card>
+      <Link to="/translator">
         <Card.Img variant="top" className="card2" />
+      </Link>  
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
-          </Card.Text>
+          <Card.Title>Inclusive Language Translator</Card.Title>
         </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
       </Card>
+
       <Card>
+      <Link to="/awarenessbot">
         <Card.Img variant="top" className="card3" />
+      </Link>    
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
+          <Card.Title>Awareness Questions Bot</Card.Title>     
+        </Card.Body>    
       </Card>
     </CardGroup>
 
