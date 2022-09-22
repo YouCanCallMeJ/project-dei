3fd66af4cadc1f0ffaac2b5ee2bfed07534bc16d
Ina Yoon <yia0319@gmail.com>
2022-09-22 00:19:30 -0400

    created Carousel with image and random quotes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,52 @@ import './App.css';
 import {Routes, Route, Link} from 'react-router-dom'
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Quote from './Quote.js'
 
 
 function App(){
   return (
     <div className="App">
 
-      <Navbar bg="dark" variant="dark">
+   
+      
+      <Routes>
+        {/* home */}
+        <Route path="/" element={<MainPg />} />   
+
+        {/* quotes section */}
+        <Route path="/quotes" element={
+          <>
+        <NavbarDefault/>
+        <Quote />
+        </>}/>
+
+        {/* translator section */}
+        <Route path="/translator" element={
+            <>
+              <NavbarDefault />
+            <div>translator 구현예정입니다</div>
+          </>}/>
+        {/* question bot section */}
+        <Route path="/awarenessbot" element={
+          <>
+          <NavbarDefault />
+          <div>awarenessbot bot 구현예정입니다</div>
+          </>}/>
+       
+      </Routes>
+
+    </div>
+
+  );
+}
+
+// Made main-page html as a MainPg component
+function MainPg(){
+  return(
+    <>
+        {/* Navbar section */}
+        <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">D E I</Navbar.Brand>
           <Nav className="me-auto">
@@ -19,9 +58,11 @@ function App(){
             <Nav.Link href="/awarenessbot">Awareness questions</Nav.Link>
           </Nav>
         </Container>
-      </Navbar>
+        </Navbar>
+      {/* Main image */}
       <div className="main-bg"></div>
 
+      {/* Card image section(3) */}
     <CardGroup>
       <Card>
         <Link to="/quotes">
@@ -50,20 +91,28 @@ function App(){
         </Card.Body>    
       </Card>
     </CardGroup>
-
-
-
-      
-      <Routes>
-        <Route path="/" element={<div>(지울예정)홈입니다</div>} />      
-       
-        <Route path="/quotes" element={<div>격려문 페이지 입니다</div>}/>
-        <Route path="/translator" element={<div>translator 구현예정입니다</div>}/>
-        <Route path="/awarenessbot" element={<div>awarenessbot bot 구현예정입니다</div>}/>
-      </Routes>
-
-    </div>
-
-  );
+  </>
+  )
 }
+
+// Made Navbar html as a NavbarDefault component
+function NavbarDefault(){
+  return(
+    <>
+    {/* Navbar section */}
+    <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="/">D E I</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/quotes">Relaxing quotes</Nav.Link>
+        <Nav.Link href="/translator">Translator</Nav.Link>
+        <Nav.Link href="/awarenessbot">Awareness questions</Nav.Link>
+      </Nav>
+    </Container>
+    </Navbar>
+    </>
+  )
+}
+
 export default App;
