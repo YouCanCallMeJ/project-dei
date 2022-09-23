a3e0e7c024cd9f266dd5475a1965242ad3cd840b
Junseo Yang <junseo.yang@bell.ca>
2022-09-22 22:11:42 -0400

    Create post request to create a question

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const QuestionModel = require('./models/Question')
 
+app.use(express.json());
+
 mongoose.connect("mongodb+srv://qwerta1234:qwerta1234@cluster0.dvd29dc.mongodb.net/awareness_questions?retryWrites=true&w=majority")
 
 app.get("/getQuestions", (req, res) => {
@@ -15,7 +17,13 @@ app.get("/getQuestions", (req, res) => {
   });
 });
 
-// app.post();
+app.post("/createQuestion", async (req, res) => {
+  const question = req.body;
+  const newQuestion = new QuestionModel(question);
+  await newQuestion.save();
+
+  res.json(question)
+});
 
 app.listen(3001, () => {
   console.log("Server is running!");
