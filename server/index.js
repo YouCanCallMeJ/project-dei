ee2989c971cf067d39cea94aee02fb7fcf7addd7
Junseo Yang <junseo.yang@bell.ca>
2022-09-22 22:05:45 -0400

    Create get request to get all questions

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const QuestionModel = require('./models/Question')
 
 mongoose.connect("mongodb+srv://qwerta1234:qwerta1234@cluster0.dvd29dc.mongodb.net/awareness_questions?retryWrites=true&w=majority")
+
+app.get("/getQuestions", (req, res) => {
+  QuestionModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// app.post();
 
 app.listen(3001, () => {
   console.log("Server is running!");
