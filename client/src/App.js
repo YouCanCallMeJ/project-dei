0e02d3fe823e88e4ed14d496af4538ea722e85af
Junseo Yang <junseo.yang@bell.ca>
2022-09-22 22:41:25 -0400

    Implement create question function

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,21 @@ import Axios from "axios";
 
 function App() {
   const [listOfQuestions, setListOfQuestions] = useState([]);
+  const [question, setQuestion] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:3001/getQuestions").then((response) => {
       setListOfQuestions(response.data);
     });
   }, []);
+
+  const createQuestion = () => {
+    Axios.post("http://localhost:3001/createQuestion", {
+        question: question
+      }).then((response) => {
+      alert("A question has been created.")
+    })
+  }
 
   return (
     <div className="App">
@@ -21,6 +30,11 @@ function App() {
             </div>
           )
         })}
+      </div>
+
+      <div>
+        <input type="text" placeholder="Enter a question!" onChange={(event) => {setQuestion(event.target.value);}}/>
+        <button onClick={createQuestion}>Create Question</button>
       </div>
     </div>
   );
