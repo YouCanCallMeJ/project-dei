29701c3370ffea95bebdf31a376b384f40909819
Branden Min <brandenmin@gmail.com>
2022-09-24 14:55:15 -0400

    Css update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,8 +5,13 @@ import NavBarElements from '../components/NavBarElements';
 const Home = () => {
   return (
     <div>
-        <h1>Home</h1>
+        <h1>DEI web</h1>
         <NavBarElements></NavBarElements>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>For diversity and inclustion</h2>
     </div>
   )
 };
