a0885743b6f25a60adf1ea2d36cf4154ef44bfc6
Branden Min <brandenmin@gmail.com>
2022-09-24 13:48:41 -0400

    Calender

--- src/components/NavBarElements.js
+++ src/components/NavBarElements.js
@@ -6,9 +6,9 @@ const NavBarElements = () => {
   return (
     <Nav defaultActiveKey="/home" className="flex-column">
     <Nav.Link className ='linkColor' href="/">Home</Nav.Link>
-    <Nav.Link className ='linkColor' href="/Bot">Bot</Nav.Link>
+    {/* <Nav.Link className ='linkColor' href="/Bot">Bot</Nav.Link> */}
     <Nav.Link className ='linkColor' href="/Holiyday">Holiyday</Nav.Link>
-    <Nav.Link className ='linkColor' href="/Motivation">Motivation</Nav.Link>
+    {/* <Nav.Link className ='linkColor' href="/Motivation">Motivation</Nav.Link> */}
     <Nav.Link className ='linkColor' href="/Translator">Translator</Nav.Link>
     
     {/* <Nav.Link eventKey="disabled" disabled>
--- src/screens/Holiyday.js
+++ src/screens/Holiyday.js
@@ -1,13 +1,24 @@
+import {useState} from 'react';
 import React from 'react'
 import NavBarElements from '../components/NavBarElements';
+import Calendar from 'react-calendar'
 
 const Holiday = () => {
+  const [date, setDate] = useState(new Date())
+
   return (
     <div>
-        <header>
-          <h1>Holiday</h1>
-          <NavBarElements></NavBarElements>
-        </header>
+      <header>
+        <h1>Calendar</h1>
+        <NavBarElements></NavBarElements>
+      </header>
+      <h1 className="header">For you all holiday</h1>
+      <div className="calendar-container">
+        <Calendar className="calCss" onChange={setDate} value={date} />
+      </div>
+      <div className="text-center">
+        Selected date: {date.toDateString()}
+      </div>
     </div>
   )
 };
